16e12aeea83792c0f307633bfccb10dfa0deff3e
Vaishnavi-Dontaraju <vaishu.dontaraju@gmail.com>
2024-02-04 18:38:30 -0400

    Resolving indentation

--- jenkins-shared-library/vars/SpringbootCIPipeline.groovy
+++ jenkins-shared-library/vars/SpringbootCIPipeline.groovy
@@ -3,23 +3,21 @@ def call(){
 }
 def pipeline(){
     pipeline {
-    agent any
-
-    stages {
-        stage('Maven Build') {
-            steps {
-                script {
-                    sh "echo Running Maven Build"
-                    sh "ls  -lrtha"
-                    sh "chmod 777 mvnw"
-                    sh "./mvnw clean install"
-                    sh "ls  -lrtha ./target"
-                    
+        agent any
+        stages {
+            stage('Maven Build') {
+                steps {
+                    script {
+                        sh "echo Running Maven Build"
+                        sh "ls  -lrtha"
+                        sh "chmod 777 mvnw"
+                        sh "./mvnw clean install"
+                        sh "ls  -lrtha ./target"
+                    }
                 }
-            }
+            }          
         }
     }
-}
 
 }
 
