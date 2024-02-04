e8df90aebde8bba4ecbb2eb0bc3bc23af3ad3b2b
Vaishnavi-Dontaraju <vaishu.dontaraju@gmail.com>
2024-02-04 19:05:36 -0400

    Updating Jenkins file

--- jenkins-shared-library/vars/SpringbootCIPipeline.groovy
+++ jenkins-shared-library/vars/SpringbootCIPipeline.groovy
@@ -1,28 +1,31 @@
+// def call() {
+//     pipeline {
+//         // Ensure that 'agent any' is specified correctly
+//         agent any
+
+//         stages {
+//             stage('Build') {
+//                 steps {
+//                     script {
+//                         echo 'Building...'
+//                         sh "echo Running Maven Build"
+//                         sh "ls  -lrtha"
+//                         sh "chmod 777 mvnw"
+//                         sh "./mvnw clean install"
+//                         sh "ls  -lrtha ./target"
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
+
 def call() {
+    echo 'Building...'
     sh "echo Running Maven Build"
     sh "ls  -lrtha"
     sh "chmod 777 mvnw"
     sh "./mvnw clean install"
     sh "ls  -lrtha ./target"
 }
-
-def pipeline() {
-    pipeline {
-        // Ensure that 'agent any' is specified correctly
-        agent any
-
-        stages {
-            stage('Build') {
-                steps {
-                    script {
-                        echo 'Building...'
-                        // Your build steps here
-                    }
-                }
-            }
-        }
-    }
-}
-
-
 
