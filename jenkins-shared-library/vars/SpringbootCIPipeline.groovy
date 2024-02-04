310398d3a50ddc778dd815f3c483c3b6b309a3e0
Vaishnavi-Dontaraju <vaishu.dontaraju@gmail.com>
2024-02-04 18:25:06 -0400

    Testing library

---
+++ jenkins-shared-library/vars/SpringbootCIPipeline.groovy
@@ -0,0 +1,28 @@
+def call(){
+    pipeline()
+}
+def pipeline(){
+    pipeline {
+    agent any
+
+    stages {
+        stage('Maven Build') {
+            steps {
+                script {
+                    build()
+                }
+            }
+        }
+    }
+}
+
+}
+
+def build(){
+    sh "echo Running Maven Build"
+    sh "ls  -lrtha"
+    sh "chmod 777 mvnw"
+    sh "./mvnw clean install"
+    sh "ls  -lrtha ./target"
+}
+
