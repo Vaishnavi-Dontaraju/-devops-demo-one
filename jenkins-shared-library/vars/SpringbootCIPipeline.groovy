b293188d2bf29e22270a62759e53b1d4563b2675
Vaishnavi-Dontaraju <vaishu.dontaraju@gmail.com>
2024-02-10 00:07:48 -0400

    Adding nexus config in shared lib

--- jenkins-shared-library/vars/SpringbootCIPipeline.groovy
+++ jenkins-shared-library/vars/SpringbootCIPipeline.groovy
@@ -7,10 +7,14 @@ def call() {
             maven 'Maven-3.9.6'
         }
 
-        // environment {
-        //     JDK_TOOL = 'AWSCorretto-21'
-        //     //MAVEN_VERSION = '3.9.6' 
-        // }
+        environment {
+            NEXUS_VERSION = "nexus3"
+            NEXUS_PROTOCOL = "http"
+            NEXUS_URL = "nexus:8081"
+            NEXUS_REPOSITORY = "mvn-group-demo"
+            NEXUS_CREDENTIAL_ID = "NexusRepo"
+            
+        }
         
         stages {
             stage('Maven Build') {
@@ -18,6 +22,45 @@ def call() {
                     mavenbuild()
                 }
             }
+
+            stage("Publish to Nexus Repository Manager") {
+            steps {
+                script {
+                    pom = readMavenPom file: "pom.xml";
+                    filesByGlob = findFiles(glob: "target/*.${pom.packaging}");
+                    echo "${filesByGlob[0].name} ${filesByGlob[0].path} ${filesByGlob[0].directory} ${filesByGlob[0].length} ${filesByGlob[0].lastModified}"
+                    artifactPath = filesByGlob[0].path;
+                    artifactExists = fileExists artifactPath;
+                    if(artifactExists) {
+                        echo "*** File: ${artifactPath}, group: ${pom.groupId}, packaging: ${pom.packaging}, version ${pom.version}";
+                        nexusArtifactUploader(
+                            nexusVersion: NEXUS_VERSION,
+                            protocol: NEXUS_PROTOCOL,
+                            nexusUrl: NEXUS_URL,
+                            groupId: pom.groupId,
+                            version: pom.version,
+                            repository: NEXUS_REPOSITORY,
+                            credentialsId: NEXUS_CREDENTIAL_ID,
+                            artifacts: [
+                                [artifactId: pom.artifactId,
+                                classifier: '',
+                                file: artifactPath,
+                                type: pom.packaging],
+                                [artifactId: pom.artifactId,
+                                classifier: '',
+                                file: "pom.xml",
+                                type: "pom"]
+                            ]
+                        );
+                    } else {
+                        error "*** File: ${artifactPath}, could not be found";
+                        }
+                    }
+                }
+
+            }
+            
+
                        
         }
     }
