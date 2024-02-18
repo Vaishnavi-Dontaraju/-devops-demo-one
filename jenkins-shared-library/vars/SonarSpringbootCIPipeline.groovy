6213466e7c4db305289c44a2bfb2578b92f01fab
Vaishnavi-Dontaraju <vaishu.dontaraju@gmail.com>
2024-02-18 17:06:35 -0400

    Creating modules

--- jenkins-shared-library/vars/SonarSpringbootCIPipeline.groovy
+++ jenkins-shared-library/vars/SonarSpringbootCIPipeline.groovy
@@ -26,71 +26,19 @@ def call() {
 
             stage('sonar quality check') {
                 steps {
-                    script {
-                        withSonarQubeEnv(credentialsId: 'Sonar-token') {
-                            sh 'mvn sonar:sonar'
-                        }
-                        /*retry(3){
-                            waitForQualityGate abortPipeline: true
-                        }*/
-                        timeout(time: 5, unit: 'MINUTES') {
-                            waitForQualityGate abortPipeline: true
-                        }
-                    }
+                    sonarQualityCheck()                 
                  
                 }
             }
             
             stage("Publish to Nexus Repository Manager") {
             steps {
-                script {
-                    pom = readMavenPom file: "pom.xml";
-                    filesByGlob = findFiles(glob: "target/*.${pom.packaging}");
-                    echo "${filesByGlob[0].name} ${filesByGlob[0].path} ${filesByGlob[0].directory} ${filesByGlob[0].length} ${filesByGlob[0].lastModified}"
-                    artifactPath = filesByGlob[0].path;
-                    artifactExists = fileExists artifactPath;
-                    if(artifactExists) {
-                        echo "*** \n File: ${artifactPath},\n group: ${pom.groupId},\n packaging: ${pom.packaging},\n version ${pom.version}";
-                        
-                        artifactVersion="${pom.version}"
-                        if(artifactVersion.toUpperCase().contains("SNAPSHOT")) {
-                            echo "Uploading artifact to Snapshot repository"
-                            NEXUS_REPOSITORY = NEXUS_SNAPSHOT_REPOSITORY
-                        } else {
-                            echo "Uploading artifact to Release repository"
-                            NEXUS_REPOSITORY = NEXUS_RELEASE_REPOSITORY
-                        }
-
-                        nexusArtifactUploader(
-                            nexusVersion: NEXUS_VERSION,
-                            protocol: NEXUS_PROTOCOL,
-                            nexusUrl: NEXUS_URL,
-                            groupId: pom.groupId,
-                            version: pom.version,
-                            repository: NEXUS_REPOSITORY,
-                            credentialsId: NEXUS_CREDENTIAL_ID,
-                            artifacts: [
-                                [artifactId: pom.artifactId,
-                                classifier: '',
-                                file: artifactPath,
-                                type: pom.packaging],
-                                [artifactId: pom.artifactId,
-                                classifier: '',
-                                file: "pom.xml",
-                                type: "pom"]
-                            ]
-                        );
-                    } else {
-                        error "*** File: ${artifactPath}, could not be found";
-                        }
-                    }
+                publishToNexus()        
                 }
-
             }
-            
-
-                       
         }
+
+
         post {
             always {
                 emailext (
@@ -102,6 +50,7 @@ def call() {
             }
             
         }
+
     }
 }
 
@@ -121,3 +70,58 @@ def mavenbuild(){
     }
 }
 
+def sonarQualityCheck(){
+    script {
+        withSonarQubeEnv(credentialsId: 'Sonar-token') {
+            sh 'mvn sonar:sonar'
+            }
+        timeout(time: 5, unit: 'MINUTES') {
+            waitForQualityGate abortPipeline: true
+        }
+    }
+}
+
+def publishToNexus(){
+    script {
+        pom = readMavenPom file: "pom.xml";
+        filesByGlob = findFiles(glob: "target/*.${pom.packaging}");
+        echo "${filesByGlob[0].name} ${filesByGlob[0].path} ${filesByGlob[0].directory} ${filesByGlob[0].length} ${filesByGlob[0].lastModified}"
+        artifactPath = filesByGlob[0].path;
+        artifactExists = fileExists artifactPath;
+        if(artifactExists) {
+            echo "*** \n File: ${artifactPath},\n group: ${pom.groupId},\n packaging: ${pom.packaging},\n version ${pom.version}";
+            
+            artifactVersion="${pom.version}"
+            if(artifactVersion.toUpperCase().contains("SNAPSHOT")) {
+                echo "Uploading artifact to Snapshot repository"
+                NEXUS_REPOSITORY = NEXUS_SNAPSHOT_REPOSITORY
+            } else {
+                echo "Uploading artifact to Release repository"
+                NEXUS_REPOSITORY = NEXUS_RELEASE_REPOSITORY
+            }
+
+            nexusArtifactUploader(
+                nexusVersion: NEXUS_VERSION,
+                protocol: NEXUS_PROTOCOL,
+                nexusUrl: NEXUS_URL,
+                groupId: pom.groupId,
+                version: pom.version,
+                repository: NEXUS_REPOSITORY,
+                credentialsId: NEXUS_CREDENTIAL_ID,
+                artifacts: [
+                    [artifactId: pom.artifactId,
+                    classifier: '',
+                    file: artifactPath,
+                    type: pom.packaging],
+                    [artifactId: pom.artifactId,
+                    classifier: '',
+                    file: "pom.xml",
+                    type: "pom"]
+                ]
+            );
+        } else {
+            error "*** File: ${artifactPath}, could not be found";
+        }
+    }
+    
+}
